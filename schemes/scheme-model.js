ed963336b190893aed40c598b4073a1f5753bc54
Natalia Beckstead <toosmiravilla@gmail.com>
2020-04-15 15:42:24 -0600

    findById and findSteps

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -2,8 +2,30 @@ const db = require('../data/db-config.js');
 
 module.exports = {
     find,
+    findById,
+    findSteps,
+    //add,
+    //addStep,
+    //update,
+    //remove
 };
 
 function find() {
     return db("schemes");
+}
+
+function findById(id) {
+    return db("schemes").where({ id }).first();
+}
+
+function findSteps(id) {
+    return db("steps")
+        .join("schemes", "schemes.id", "steps.scheme_id")
+		.select(
+            "schemes.scheme_name",
+            "steps.step_number",
+			"steps.instructions"
+        )
+        .orderBy('step_number', 'asc')
+		.where({ scheme_id: id });
 }
